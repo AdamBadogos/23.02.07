fb354a0e90960a79adca1ff35926e7f9e320c564
Boysmailp <97170109+Boysmailp@users.noreply.github.com>
2023-02-07 11:16:43 +0100

    4

--- laptop.py
+++ laptop.py
@@ -1,3 +1,29 @@
 f=open("gep.txt","r")
 
-asd
+file_data=[]
+
+for i in file: 
+    if(i[-1=="\n"]):
+        file_data.append(i[:-1].split("\t"))
+    else:
+        file_data.append(i.split("\t"))
+
+del file_data [0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
